6d537f95ec2e8a144652f1b7c9aedc202ce7dd7d
wongco <ginson@gmail.com>
2018-09-12 23:34:29 -0700

    Update letterTally recursive function

--- src/recursion.js
+++ src/recursion.js
@@ -534,6 +534,21 @@ var flatten = function(array) {
 // 31. Given a string, return an object containing tallies of each letter.
 // letterTally('potato'); // {p:1, o:2, t:2, a:1}
 var letterTally = function(str, obj) {
+	if(str.length === 0) {
+		return obj;
+	}
+
+  if(typeof obj === 'object') {
+    if(obj.hasOwnProperty(str[0]) === false) {
+      obj[str[0]] = 1;
+    } else {
+      obj[str[0]]++;
+    }
+    return letterTally(str.slice(1), obj);    
+  }
+  var newObj = {};
+  newObj[str[0]] = 1;
+  return letterTally(str.slice(1), newObj);
 };
 
 // 32. Eliminate consecutive duplicates in a list. If the list contains repeated
